b7dbeb0c0472439b6bb61caefa86ccb8052d07ff
Bruno Hubner <brunohubnerdev@gmail.com>
2021-11-02 17:06:10 -0300

    Create static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import Navbar from "components/Navbar"
+import Footer from "components/Footer"
+import DataTable from "components/DataTable"
+
 export default function App() {
     return (
-        <div className="App">
-            <h1 className="text-primary">App</h1>
-        </div>
+        <>
+            <Navbar></Navbar>
+            <div className="container">
+                <h1 className="text-primary">DS vendas</h1>
+                <DataTable></DataTable>
+            </div>
+            <Footer></Footer>
+        </>
     )
 }
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">
+                    desenvolvido por &nbsp;
+                    <a href="https://github.com/brunohubner" 
+                        target="_blank" 
+                        rel="noreferrer">Bruno Hubner</a>
+                </p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    )
+}
