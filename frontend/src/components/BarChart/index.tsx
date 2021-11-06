36f6819bd2b8d3c4f4936673819343d3c513e15b
Bruno Hubner <brunohubnerdev@gmail.com>
2021-11-06 07:17:18 -0300

    BarChart integration API

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,38 +1,71 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import Chart from "react-apexcharts"
+import { SaleSuccess } from "types/sale"
+import { BASE_URL } from "utils/request"
+import { round } from "utils/round"
 
-export type BarChartData = {
+type SeriesData = {
+    name: string
+    data: number[]
+}
+
+type BarChartData = {
     labels: { categories: string[] }
-    series: {
-        name: string
-        data: number[]
-    }
+    series: SeriesData[]
+}
+
+const INITIAL_BAR_CHART_STATE: BarChartData = {
+    labels: { categories: [] },
+    series: [{
+        name: "",
+        data: []
+    }]
 }
 
 export default function BarChart() {
+    const [barChartData, setBarChartData] = useState<BarChartData>(INITIAL_BAR_CHART_STATE)
+
     const options = {
         plotOptions: {
             bar: {
                 horizontal: true,
             }
         },
-    };
-    
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };
+    }
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+            .then(resp => {
+                const data = resp.data as SaleSuccess[]
+                const labels = data.map(x => x.sellerName)
+                const series = data.map(x => round(x.deals / x.visited * 100, 1))
+                setBarChartData({
+                    labels: { categories: labels },
+                    series: [{
+                        name: "% Sucesso",
+                        data: series
+                    }]
+                })
+            })
+    }, [])
+
+    // const mockData = {
+    //     labels: {
+    //         categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //     },
+    //     series: [
+    //         {
+    //             name: "% Sucesso",
+    //             data: [43.6, 67.1, 67.7, 45.6, 71.1]
+    //         }
+    //     ]
+    // };
 
     return (
-        <Chart 
-            options={{ ...options, xaxis: mockData.labels }}
-            series={mockData.series}
+        <Chart
+            options={{ ...options, xaxis: barChartData.labels }}
+            series={barChartData.series}
             type="bar"
             height={240} ></Chart>
     )
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -9,22 +9,23 @@ type DonutChartData = {
     series: number[]
 }
 
-const INITIAL_STATE: DonutChartData = {
+const INITIAL_DONUT_CHART_STATE: DonutChartData = {
     labels: [],
     series: []
-} 
+}
 
 export default function DonutChart() {
-    const [donutChartData, setDonutChartData] = useState<DonutChartData>(INITIAL_STATE)
+    const [donutChartData, setDonutChartData] = useState<DonutChartData>(INITIAL_DONUT_CHART_STATE)
 
+    
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-            .then(resp => {
-                const data = resp.data as SaleSum[]
-                const labels = data.map(x => x.sellerName)
-                const series = data.map(x => x.sum)
-                setDonutChartData({ labels, series })
-            })
+        .then(resp => {
+            const data = resp.data as SaleSum[]
+            const labels = data.map(x => x.sellerName)
+            const series = data.map(x => x.sum)
+            setDonutChartData({ labels, series })
+        })
     }, [])
     
     const options = {
@@ -32,6 +33,11 @@ export default function DonutChart() {
             show: true
         }
     }
+
+    // const mockData = {
+    //     series: [477138, 499928, 444867, 220426, 473088],
+    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    // }
 
     return (
         <Chart 
