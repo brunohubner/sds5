78a5fe1a773b5774b1e96276624b15e63df11ed7
Bruno Hubner <brunohubnerdev@gmail.com>
2021-11-19 19:44:00 -0300

    Update footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ function Footer() {
         <footer className="footer mt-auto pt-3 pb-1 bg-dark">
             <div className="container">
                 <p className="text-light ">
-                    por &nbsp;
+                    Copyright &copy; 2021&nbsp;
                     <a href="https://github.com/brunohubner" 
                         target="_blank" 
                         rel="noreferrer">Bruno Hubner</a></p>
